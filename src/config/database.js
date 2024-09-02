ab756f4832f6cc211d602909c31cc1b8301fbdea
afmcmorais <afmcmorais@icloud.com>
2024-09-02 16:48:11 -0300

    update

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '230117',
+    database: 'usersdb',
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true
+    }
+}
